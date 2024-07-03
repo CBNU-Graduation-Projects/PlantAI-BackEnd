7dbd5237ba35d556c6adcb235d91662da220250e
Mina Dong <minadong404@gmail.com>
2024-07-03 15:08:55 +0900

    로그인창 추가

--- plant_web_app/src/pages/Home.js
+++ plant_web_app/src/pages/Home.js
@@ -9,44 +9,6 @@ const Home = () => {
     <div>
       <h1>식물상태 진단 시스템</h1>
 
-      <form onSubmit={handleSubmit}>
-        {/* <div>
-        <Link to="/upload">[ 드래그에서 사진 입력 (multer 넣기) ]</Link>
-        </div>
-        <div>
-        <h2> 식물정보입력 칸 </h2>
-        <table>
-          <tbody>
-            <tr>
-              <th>식물종류선택</th>
-              <td>식물종류데이터</td>
-            </tr>
-
-            <tr>
-              <th>관수 주기 입력</th>
-              <td>데이터2</td>
-            </tr>
-
-            <tr>
-              <th>광선량 입력</th>
-              <td>데이터3</td>
-            </tr>
-
-            <tr>
-              <th>토양 환경 입력</th>
-              <td>데이터4</td>
-            </tr>
-          </tbody>
-        </table>
-        </div> */}
-      
-
-      {/* 구분선 hr */}
-      <hr />
-      <div>
-        <button type='submit'>[ 식물 상태 진단 ]</button>
-      </div>
-      </form>
     </div>
   );
 };
--- plant_web_app/src/pages/Login.js
+++ plant_web_app/src/pages/Login.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import './login.css'
+
 
 const Login = () => {
-  return (
-    <div>
-      <h1>관리자 로그인</h1>
-     </div>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-     //로그인창 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('로그인 시도:', { username, password });
+  };
+
+  return (
+    <div className="login-container">
+      <h1>관리자 로그인</h1>
+      <form className= "login-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">사용자 이름</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">로그인</button>
+      </form>
+    </div>
   );
 };
 
