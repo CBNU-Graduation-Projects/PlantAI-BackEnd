f9261c9b37b892da9eb34cf90348d6eec4de54aa
Mina Dong <minadong404@gmail.com>
2024-10-11 22:10:59 +0900

    login 엔드포인트 추가

--- plant_web_app/src/pages/Login.js
+++ plant_web_app/src/pages/Login.js
@@ -1,20 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import './login.css'
-
+import React, { useState, useEffect } from "react";
+import "./login.css";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('로그인 시도:', { username, password });
+    //console.log('로그인 시도:', { username, password });
+
+    // 로그인 요청 보내기
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // 로그인 성공
+          alert("로그인 성공");
+        } else {
+          // 로그인 실패
+          throw new Error("로그인 실패");
+        }
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
   };
 
   return (
     <div className="login-container">
       <h1>관리자 로그인</h1>
-      <form className= "login-form" onSubmit={handleSubmit}>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <form className="login-form" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="username">사용자 이름</label>
           <input
